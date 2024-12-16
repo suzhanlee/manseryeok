cb2728ce0a39a74aa94626a809e4b2f6ac573fdc
suzhanlee <108535068+suzhanlee@users.noreply.github.com>
2024-12-16 23:29:49 +0900

    feat : main 구성

---
+++ src/main/java/com/example/manseryeok/ManseryeokApplication.java
@@ -0,0 +1,13 @@
+package com.example.manseryeok;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManseryeokApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManseryeokApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/manseryeok/ManseryeokApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.manseryeok;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManseryeokApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
