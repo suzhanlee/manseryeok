c0c4cc259557073d503a088ebc6c15b2437ead83
suzhanlee <108535068+suzhanlee@users.noreply.github.com>
2024-12-17 15:09:57 +0900

    feat : 양력, 음력으로 캘린더 타입을 찾는 기능 추가

---
+++ src/main/java/com/example/manseryeok/entity/CalendarType.java
@@ -0,0 +1,21 @@
+package com.example.manseryeok.entity;
+
+import java.util.Arrays;
+
+public enum CalendarType {
+
+    SOLAR("양력"), LUNAR("음력");
+
+    private final String name;
+
+    CalendarType(String name) {
+        this.name = name;
+    }
+
+    public static CalendarType findByName(String name) {
+        return Arrays.stream(values())
+                .filter(type -> type.name.equals(name))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("타입을 찾을 수 없습니다 : " + name));
+    }
+}
